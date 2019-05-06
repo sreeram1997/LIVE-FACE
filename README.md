d4ab93572b5a8188c327fe43d7d43122eea3d999
Sreeram Gunasekaran <30369979+sreeram1997@users.noreply.github.com>
2019-05-06 15:07:06 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,22 @@
+# Face_recognition
+Recognition of face live from webcam
+
+# Methodology
+- Haar cascade to detect face in frame
+- OpenCV's LBPHFaceRecognizer to recognized face
+
+# Steps to produce Result
+- Clone entire repository
+- Goto folder in which cloning is done 
+> Create a folder named as "image" 
+> Your image folder should like..
+![sc1](https://user-images.githubusercontent.com/36638657/44214919-d671a480-a18e-11e8-9382-8b3a2e2cea25.png)
+- Goto cmd/powershell type
+> python face_rec_cv.py
+- After execution completed type following command
+> python face_det_cv.py
+- Volaa !!
+
+
+# Video 
+Watch Youtube video on link: https://youtu.be/zhaJdgAmnPE
---
+++ face_rec_cv.py
@@ -0,0 +1,49 @@
+import cv2 
+import os 
+import numpy as np 
+from PIL import Image
+import pickle 
+import imutils
+
+Base_dir = os.path.dirname(os.path.abspath(__file__))
+image_path = os.path.join(Base_dir,'image')
+
+face_cascade = cv2.CascadeClassifier('C:/Users/HP/AppData/Local/Programs/Python/Python37/Lib/site-packages/cv2/data/haarcascade_frontalface_default.xml')
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+
+y_label = []
+x_train = []
+
+current_id = 0
+label_ids = {}
+
+for root,dirname,filename in os.walk(image_path):
+    for file in filename:
+        path = os.path.join(root,file)
+        label = os.path.basename(root).replace(" ","-").lower()
+        #print(label,path)
+
+        if not label in label_ids:
+            label_ids[label] = current_id
+            current_id += 1
+        
+        id_ = label_ids[label]
+        
+        pil_image = Image.open(path).convert('L')
+    
+        # pil_image = imutils.resize(pil_image,width=600)
+        image_array = np.array(pil_image,dtype='uint8')
+        image_array = imutils.resize(image_array,width=600)
+        
+        faces = face_cascade.detectMultiScale(image_array,1.5,5)
+        
+        for (x,y,w,h) in faces:
+            roi = image_array[y:y+h,x:x+w]
+            x_train.append(roi)
+            y_label.append(id_)
+
+with open("label.pickle",'wb') as f:
+    pickle.dump(label_ids,f)
+
+recognizer.train(x_train,np.array(y_label))
+recognizer.save("trainner.yml")
